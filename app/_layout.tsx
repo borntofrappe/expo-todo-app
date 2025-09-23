a07102eea4c71f738c352e1ec8961fffbdf5bc67
Gabriele Corti <borntofrappe@gmail.com>
2025-09-23 09:32:51 +0200

    tab transition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,42 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Dimensions } from "react-native";
+import { gray } from "tailwindcss/colors";
 import "../styles/global.css";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabsLayout() {
+  const backgroundColor = gray[50];
+  const transitionDuration = 280;
+
+  return (
+    <Tabs
+      screenOptions={{
+        sceneStyle: {
+          backgroundColor,
+        },
+        headerShown: false,
+        tabBarStyle: {
+          // display: "none", // uncomment when handling navigation between the two tabs
+        },
+        transitionSpec: {
+          animation: "timing",
+          config: {
+            duration: transitionDuration,
+          },
+        },
+        sceneStyleInterpolator: ({ current }) => {
+          const { width } = Dimensions.get("window");
+
+          const translateX = current.progress.interpolate({
+            inputRange: [-1, 0, 1],
+            outputRange: [-width, 0, width],
+          });
+          return {
+            sceneStyle: {
+              transform: [{ translateX }],
+            },
+          };
+        },
+      }}
+    ></Tabs>
+  );
 }
--- app/settings.tsx
+++ app/settings.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Settings() {
   return (
     <View className="flex-1 w-full max-w-[600px] mx-auto">
-      <Text className="text-3xl font-light">Tasks</Text>
+      <Text className="text-3xl font-light">Settings</Text>
     </View>
   );
 }
