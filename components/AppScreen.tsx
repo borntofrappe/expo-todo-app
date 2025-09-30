d1216b60c90c7d903c714706f2774bf97731d604
Gabriele Corti <borntofrappe@gmail.com>
2025-09-30 09:32:04 +0200

    safe area view

--- components/AppScreen.tsx
+++ components/AppScreen.tsx
@@ -1,13 +1,15 @@
 import { ThemeContext } from "@/context/ThemeContext";
 import { PropsWithChildren, useContext } from "react";
-import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppScreen({ children }: PropsWithChildren) {
   const context = useContext(ThemeContext);
 
   return (
-    <View className={`${context && context.theme} flex-1 bg-background-1`}>
-      <View className="flex-1 w-full max-w-[600px] mx-auto">{children}</View>
-    </View>
+    <SafeAreaView
+      className={`${context && context.theme} flex-1 bg-background-1`}
+    >
+      {children}
+    </SafeAreaView>
   );
 }
